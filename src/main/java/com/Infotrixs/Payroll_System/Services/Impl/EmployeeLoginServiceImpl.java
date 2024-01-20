a3b8c0e7e32e8d9818c96f83f6f689c32c7346e0
Salik Anwar <infernosight@gmail.com>
2024-01-20 13:04:49 +0530

    All APIs done on my part

--- src/main/java/com/Infotrixs/Payroll_System/Services/Impl/EmployeeLoginServiceImpl.java
+++ src/main/java/com/Infotrixs/Payroll_System/Services/Impl/EmployeeLoginServiceImpl.java
@@ -1,8 +1,37 @@
 package com.Infotrixs.Payroll_System.Services.Impl;
 
+import com.Infotrixs.Payroll_System.DTOs.Outgoing.EmployeeLoginDetails;
+import com.Infotrixs.Payroll_System.Exceptions.InvalidInputException;
+import com.Infotrixs.Payroll_System.Models.Employee;
+import com.Infotrixs.Payroll_System.Repositories.EmployeeRepository;
 import com.Infotrixs.Payroll_System.Services.EmployeeLoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeLoginServiceImpl implements EmployeeLoginService {
+    final EmployeeRepository employeeRepository;
+    @Autowired
+    public EmployeeLoginServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public EmployeeLoginDetails employeeLogin() {
+        // extract employee username from the authentication details
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication == null){
+            throw  new InvalidInputException("Invalid username or password.");
+        }
+        String employeeUsername = authentication.getName();
+        // get employee
+        Employee employee = employeeRepository.findByUsername(employeeUsername);
+        // return name and ID
+        return EmployeeLoginDetails.builder()
+                .employeeId(employee.getEmpId())
+                .employeeName(employee.getName())
+                .build();
+    }
 }
